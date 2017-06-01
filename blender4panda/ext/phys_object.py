4410f1fad169d0916d6262ef0c661d0097255302
Will Alcorn <alcornwill@gmail.com>
2017-06-01 07:48:07 +0100

    disable physics

--- blender4panda/ext/phys_object.py
+++ blender4panda/ext/phys_object.py
@@ -67,81 +67,81 @@ def make_collision_bounds_shape(scene, obj, dynamic=True):
 
 
 def invoke(scene, obj, action):
-
-    if action == 'LOAD':
-
-        node = BulletRigidBodyNode(obj['name'])
-        shape = None
-        if obj['phys_type'] == 'STATIC':
-            if 'phys_collision_bounds' in obj:
-                shape = make_collision_bounds_shape(scene, obj, dynamic=False)
-            else:
-                if obj['type'] == 'MESH':
-                    mesh = make_bullet_mesh(scene, BulletTriangleMesh, scene.meshes[obj['name']])
-                    shape = BulletTriangleMeshShape(mesh, dynamic=False)
-
-        elif obj['phys_type'] == 'RIGID_BODY':
-            if 'phys_collision_bounds' in obj:
-                shape = make_collision_bounds_shape(scene, obj)
-            else:
-                shape = BulletSphereShape(obj['phys_radius'])
-
-            node.setMass(obj['phys_mass'])
-
-
-        if shape:
-            node.addShape(shape)
-
-            if 'phys_mat_order' in obj and obj['phys_mat_order']:# and not 'phys_collision_bounds' in obj:
-                for m_name in obj['phys_mat_order']:
-                    mat = scene.data_dict['materials'][m_name]
-                    if mat['use_physics']:
-                        node.set_friction(mat['phys_friction'])
-                        node.set_restitution(mat['phys_elasticity'])
-                        break
-            else:
-                node.set_friction(1.0)
-
-            # Sleepeng (deactivation) options
-            if obj['phys_deactivation']:
-                scene_data = scene.data_dict['scene']
-                node.set_angular_sleep_threshold(scene_data['phys_deactivation_angular_threshold'])
-                node.set_linear_sleep_threshold(scene_data['phys_deactivation_linear_threshold'])
-                node.set_deactivation_time(scene_data['phys_deactivation_time'])
-                node.set_active(True)
-            else:
-                node.set_deactivation_enabled(False)
-
-
-            if 'phys_friction_coefficients' in obj:
-                node.set_anisotropic_friction(Vec3(*obj['phys_friction_coefficients']))
-
-            # Linear and angular damping
-            node.set_linear_damping(obj['phys_linear_damping'])
-            node.set_angular_damping(obj['phys_angular_damping'])
-            #node.set_inertia(0.9)
-
-            # Linear and angular locking
-            if True in obj['phys_lock_location']:
-                v = Vec3(*map(int, obj['phys_lock_location']))
-                node.set_linear_factor(v)
-            if True in obj['phys_lock_rotation']:
-                v = Vec3(*map(int, obj['phys_lock_rotation']))
-                node.set_angular_factor(v)
-
-            np = scene.root.attachNewNode(node)
-
-            mask = BitMask32()
-            for i,val in enumerate(obj['phys_collision_mask']):
-                if val: mask.set_bit(i)
-            np.set_collide_mask(mask)
-
-            np.setMat(scene.meshes[obj['name']].getMat())
-
-            scene.meshes[obj['name']].wrtReparentTo(np)
-            scene.phys_world.attachRigidBody(node)
-
-            scene.objects[obj['name']] = np
-
-        if obj['invisible']:
-            scene.meshes[obj['name']].hide()
+    return
+    # if action == 'LOAD':
+    #
+    #     node = BulletRigidBodyNode(obj['name'])
+    #     shape = None
+    #     if obj['phys_type'] == 'STATIC':
+    #         if 'phys_collision_bounds' in obj:
+    #             shape = make_collision_bounds_shape(scene, obj, dynamic=False)
+    #         else:
+    #             if obj['type'] == 'MESH':
+    #                 mesh = make_bullet_mesh(scene, BulletTriangleMesh, scene.meshes[obj['name']])
+    #                 shape = BulletTriangleMeshShape(mesh, dynamic=False)
+    #
+    #     elif obj['phys_type'] == 'RIGID_BODY':
+    #         if 'phys_collision_bounds' in obj:
+    #             shape = make_collision_bounds_shape(scene, obj)
+    #         else:
+    #             shape = BulletSphereShape(obj['phys_radius'])
+    #
+    #         node.setMass(obj['phys_mass'])
+    #
+    #
+    #     if shape:
+    #         node.addShape(shape)
+    #
+    #         if 'phys_mat_order' in obj and obj['phys_mat_order']:# and not 'phys_collision_bounds' in obj:
+    #             for m_name in obj['phys_mat_order']:
+    #                 mat = scene.data_dict['materials'][m_name]
+    #                 if mat['use_physics']:
+    #                     node.set_friction(mat['phys_friction'])
+    #                     node.set_restitution(mat['phys_elasticity'])
+    #                     break
+    #         else:
+    #             node.set_friction(1.0)
+    #
+    #         # Sleepeng (deactivation) options
+    #         if obj['phys_deactivation']:
+    #             scene_data = scene.data_dict['scene']
+    #             node.set_angular_sleep_threshold(scene_data['phys_deactivation_angular_threshold'])
+    #             node.set_linear_sleep_threshold(scene_data['phys_deactivation_linear_threshold'])
+    #             node.set_deactivation_time(scene_data['phys_deactivation_time'])
+    #             node.set_active(True)
+    #         else:
+    #             node.set_deactivation_enabled(False)
+    #
+    #
+    #         if 'phys_friction_coefficients' in obj:
+    #             node.set_anisotropic_friction(Vec3(*obj['phys_friction_coefficients']))
+    #
+    #         # Linear and angular damping
+    #         node.set_linear_damping(obj['phys_linear_damping'])
+    #         node.set_angular_damping(obj['phys_angular_damping'])
+    #         #node.set_inertia(0.9)
+    #
+    #         # Linear and angular locking
+    #         if True in obj['phys_lock_location']:
+    #             v = Vec3(*map(int, obj['phys_lock_location']))
+    #             node.set_linear_factor(v)
+    #         if True in obj['phys_lock_rotation']:
+    #             v = Vec3(*map(int, obj['phys_lock_rotation']))
+    #             node.set_angular_factor(v)
+    #
+    #         np = scene.root.attachNewNode(node)
+    #
+    #         mask = BitMask32()
+    #         for i,val in enumerate(obj['phys_collision_mask']):
+    #             if val: mask.set_bit(i)
+    #         np.set_collide_mask(mask)
+    #
+    #         np.setMat(scene.meshes[obj['name']].getMat())
+    #
+    #         scene.meshes[obj['name']].wrtReparentTo(np)
+    #         scene.phys_world.attachRigidBody(node)
+    #
+    #         scene.objects[obj['name']] = np
+    #
+    #     if obj['invisible']:
+    #         scene.meshes[obj['name']].hide()
